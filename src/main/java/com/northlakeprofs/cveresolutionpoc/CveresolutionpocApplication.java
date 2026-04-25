cbc07321b0ca42787bf171966a25420ae3e74e44
venkatkolan <kvr248@gmail.com>
2026-04-25 11:17:13 -0500

    inital commit

---
+++ src/main/java/com/northlakeprofs/cveresolutionpoc/CveresolutionpocApplication.java
@@ -0,0 +1,13 @@
+package com.northlakeprofs.cveresolutionpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CveresolutionpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CveresolutionpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northlakeprofs/cveresolutionpoc/CveresolutionpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northlakeprofs.cveresolutionpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CveresolutionpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
